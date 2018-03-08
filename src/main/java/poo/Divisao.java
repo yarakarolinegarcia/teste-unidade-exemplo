cb6292aa89bc9fed94684859329c673fabe90864
yarakarolinegarcia <yarakarolinegarcia@gmail.com>
2018-03-08 17:39:47 -0300

    Teste divisao

---
+++ src/main/java/poo/Divisao.java
@@ -0,0 +1,16 @@
+package poo;
+
+public class Divisao {
+
+                /**
+                 * Faz a divisao de dois inteiros
+                 * @param a operando 1
+                 * @param b operando 2
+                 * @return resultado da divisao dos operandos
+                 */
+                public double divisao(int a, int b){
+                        return (double)(a/b);
+                }
+
+        }
+
---
+++ src/test/java/poo/TesteDivisao.java
@@ -0,0 +1,37 @@
+package poo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Dictionary;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class TesteDivisao {
+    @Parameterized.Parameters(name = "{index}: divisao({0},{1}) = {2}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { 2, 2, 1 },
+                {22, 2, 11 },
+                {-2, 2, -1 },
+
+        });
+    }
+    @Parameterized.Parameter(0)
+    public int operandoUm;
+
+    @Parameterized.Parameter(1)
+    public int operandoDois;
+
+    @Parameterized.Parameter(2)
+    public int resultado;
+
+    @Test
+    public void teste(){
+        Divisao obj = new Divisao();
+        assertEquals("resultado errado.",resultado, obj.divisao(operandoUm, operandoDois),5);
+    }
+}
